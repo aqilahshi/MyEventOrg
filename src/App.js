faf7d8ff03f2adccb2643685d5decf217692bfb1
zakirahsharifuddin <zakirahsharifuddin@student.usm.my>
2023-06-03 03:12:06 +0800

    Send role to next page

--- src/App.js
+++ src/App.js
@@ -7,91 +7,123 @@ import ParticipantsPage from './component/login/ParticipantsPage';
 import AdminPage from './component/login/AdminPage';
 import HomePage from './component/login/HomePage';
 import LoginPage from './component/login/LogIn';
+import './component/login/loginstyle.css';
 
 function App() {
-  const [userRole, setUserRole] = useState(null);
+  // const [userRole, setUserRole] = useState(null);
 
-  const handleLogin = (role) => {
-    setUserRole(role);
-  };
+  // const handleLogin = (role) => {
+  //   setUserRole(role);
+  // };
 
-  const handleLogout = () => {
-    setUserRole(null);
-  };
+  // const handleLogout = () => {
+  //   setUserRole(null);
+  // };
 
-  const ProtectedRoute = ({ path, element, allowedRoles }) => {
-    if (allowedRoles.includes(userRole)) {
-      return element;
-    } else {
-      return <Navigate to="/" replace />;
-    }
-  };
+  // const ProtectedRoute = ({ path, element, allowedRoles }) => {
+  //   if (allowedRoles.includes(userRole)) {
+  //     return element;
+  //   } else {
+  //     return <Navigate to="/" replace />;
+  //   }
+  // };
 
-  const handleRoleSelection = (role) => {
-    // Handle the role selection logic
-    console.log('Selected role:', role);
-  };
+  // const handleRoleSelection = (role) => {
+  //   // Handle the role selection logic
+  //   console.log('Selected role:', role);
+  // };
+
+  // return (
+  //   <Router>
+  //     <Routes>
+  //       <Route
+  //         path="/"
+  //         element={<HomePage handleLogin={handleRoleSelection} />}
+  //       />
+  //       <Route
+  //         path="/login"
+  //         element={<LoginPage handleRoleSelection={handleRoleSelection} />}
+  //       />
+  //       <Route
+  //         path="/vendor"
+  //         element={
+  //           <ProtectedRoute
+  //             path="/"
+  //             element={<VendorPage handleLogout={handleLogout} />}
+  //             allowedRoles={['vendor']}
+  //           />
+  //         }
+  //       />
+  //       <Route
+  //         path="/committee"
+  //         element={
+  //           <ProtectedRoute
+  //             path="/"
+  //             element={<CommitteePage handleLogout={handleLogout} />}
+  //             allowedRoles={['committee']}
+  //           />
+  //         }
+  //       />
+  //       <Route
+  //         path="/lecturers"
+  //         element={
+  //           <ProtectedRoute
+  //             path="/"
+  //             element={<LecturersPage handleLogout={handleLogout} />}
+  //             allowedRoles={['lecturers']}
+  //           />
+  //         }
+  //       />
+  //       <Route
+  //         path="/participants"
+  //         element={
+  //           <ProtectedRoute
+  //             path="/"
+  //             element={<ParticipantsPage handleLogout={handleLogout} />}
+  //             allowedRoles={['participants']}
+  //           />
+  //         }
+  //       />
+  //       <Route
+  //         path="/admin"
+  //         element={
+  //           <ProtectedRoute
+  //             path="/"
+  //             element={<AdminPage handleLogout={handleLogout} />}
+  //             allowedRoles={['admin']}
+  //           />
+  //         }
+  //       />
+  //     </Routes>
+  //   </Router>
+
+
+ 
+
 
   return (
     <Router>
       <Routes>
         <Route
-          path="/"
-          element={<HomePage handleLogin={handleRoleSelection} />}
+          path="/" element={<HomePage/>}
         />
         <Route
-          path="/login"
-          element={<LoginPage handleRoleSelection={handleRoleSelection} />}
+          path="/login" element={<LoginPage/>}
         />
         <Route
-          path="/vendor"
-          element={
-            <ProtectedRoute
-              path="/"
-              element={<VendorPage handleLogout={handleLogout} />}
-              allowedRoles={['vendor']}
-            />
-          }
+          path="/vendor" element={<VendorPage/>}
         />
         <Route
-          path="/committee"
-          element={
-            <ProtectedRoute
-              path="/"
-              element={<CommitteePage handleLogout={handleLogout} />}
-              allowedRoles={['committee']}
-            />
-          }
+          path="/committee" element={<CommitteePage/>}
         />
         <Route
-          path="/lecturers"
-          element={
-            <ProtectedRoute
-              path="/"
-              element={<LecturersPage handleLogout={handleLogout} />}
-              allowedRoles={['lecturers']}
-            />
-          }
+          path="/lecturers" element={<LecturersPage/>}
         />
         <Route
-          path="/participants"
-          element={
-            <ProtectedRoute
-              path="/"
-              element={<ParticipantsPage handleLogout={handleLogout} />}
-              allowedRoles={['participants']}
-            />
-          }
+          path="/participants" element={<ParticipantsPage/>}
         />
         <Route
-          path="/admin"
-          element={
-            <ProtectedRoute
-              path="/"
-              element={<AdminPage handleLogout={handleLogout} />}
-              allowedRoles={['admin']}
-            />
-          }
+          path="/admin" element={<AdminPage/>}
         />
       </Routes>
     </Router>
