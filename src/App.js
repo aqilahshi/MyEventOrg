70b9841086fc5c13d768645416eb97293008e81d
Nadhirah Halim <nadna031@gmail.com>
2023-06-02 17:09:53 +0800

    Update App.js
    Update App.js for log in purposes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import VendorPage from './navbarpart/VendorPage';
+import CommitteePage from './navbarpart/CommitteePage';
+import LecturersPage from './navbarpart/LecturersPage';
+import ParticipantsPage from './navbarpart/ParticipantsPage';
+import AdminPage from './navbarpart/AdminPage';
+import HomePage from './navbarpart/HomePage';
 
 function App() {
+  const [userRole, setUserRole] = useState(null);
+
+  const handleLogin = (role) => {
+    setUserRole(role);
+  };
+
+  const handleLogout = () => {
+    setUserRole(null);
+  };
+
+  const ProtectedRoute = ({ path, element, allowedRoles }) => {
+    if (allowedRoles.includes(userRole)) {
+      return element;
+    } else {
+      return <Navigate to="/" replace />;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <HomePage handleLogin={handleLogin} />
+          }
+        />
+        <Route
+          path="/vendor"
+          element={
+            <ProtectedRoute
+              path="/"
+              element={<VendorPage handleLogout={handleLogout} />}
+              allowedRoles={['vendor']}
+            />
+          }
+        />
+        <Route
+          path="/committee"
+          element={
+            <ProtectedRoute
+              path="/"
+              element={<CommitteePage handleLogout={handleLogout} />}
+              allowedRoles={['committee']}
+            />
+          }
+        />
+        <Route
+          path="/lecturers"
+          element={
+            <ProtectedRoute
+              path="/"
+              element={<LecturersPage handleLogout={handleLogout} />}
+              allowedRoles={['lecturers']}
+            />
+          }
+        />
+        <Route
+          path="/participants"
+          element={
+            <ProtectedRoute
+              path="/"
+              element={<ParticipantsPage handleLogout={handleLogout} />}
+              allowedRoles={['participants']}
+            />
+          }
+        />
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute
+              path="/"
+              element={<AdminPage handleLogout={handleLogout} />}
+              allowedRoles={['admin']}
+            />
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
+
